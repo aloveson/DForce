cff1e4be77c007a8e602a9744963f9d195a6af5a
rthuid <rthuid@gmail.com>
2017-10-18 15:32:06 +0530

    parallax

--- js/parallax.js
+++ js/parallax.js
@@ -10,9 +10,20 @@ $.fn.parallax = function (resistance, mouse) {
 
 };
 $(document).mousemove(function (e) {
-    $('.anim-1').parallax(5, e);
-    $('.anim-2').parallax(20, e);
-    $('.anim-3').parallax(-30, e);
+    $('.anim-0').parallax(40, e);
+    $('.anim-1').parallax(35, e);
+    $('.anim-2').parallax(30, e);
+    $('.anim-3').parallax(25, e);
+    $('.anim-4').parallax(20, e);
+    $('.anim-5').parallax(6, e);
+    $('.anim-6').parallax(5, e);
+    $('.anim-7').parallax(4, e);
+    $('.anim-8').parallax(-10, e);
+    $('.anim-9').parallax(-15, e);
+    $('.anim-10').parallax(-20, e);
+    $('.anim-11').parallax(-25, e);
+    $('.anim-12').parallax(-30, e);
+    $('.anim-13').parallax(-35, e);
     
 });
 
